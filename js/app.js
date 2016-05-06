d103ba1f1eeac4ac833839669cad723297981b1d
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2016-05-06 14:03:30 +0200

    Poprawiona wersja strony

---
+++ js/app.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+// ------------------------DROP DOWN MENU-----------------------------------
+    // var listElements = document.querySelectorAll(".drop_down ul li");
+    //
+    // for (var i = 0; i < listElements.length; i++) {
+    //     listElements[i].addEventListener("mouseover", function(event) {
+    //         if (this.children.length > 0) {
+    //             var elementToShow = this.children[0];
+    //             elementToShow.style.display = "block";
+    //         }
+    //     });
+    //
+    //     listElements[i].addEventListener("mouseout", function(event) {
+    //         if (this.children.length > 0) {
+    //             var elementToHide = this.children[0];
+    //             elementToHide.style.display = "none";
+    //         }
+    //
+    //     });
+    // }
+    //
+
+
+
+
+
+
+
+});
