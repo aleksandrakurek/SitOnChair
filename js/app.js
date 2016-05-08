9c08013c1607d8e8da498c12b6e1cc6dbbdb9459
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2016-05-08 18:28:22 +0200

    Dodany znikający opis w galerii

--- js/app.js
+++ js/app.js
@@ -1,26 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    // GALLERY - znikajacy element z nazwa
 
-// ------------------------DROP DOWN MENU-----------------------------------
-    // var listElements = document.querySelectorAll(".drop_down ul li");
-    //
-    // for (var i = 0; i < listElements.length; i++) {
-    //     listElements[i].addEventListener("mouseover", function(event) {
-    //         if (this.children.length > 0) {
-    //             var elementToShow = this.children[0];
-    //             elementToShow.style.display = "block";
-    //         }
-    //     });
-    //
-    //     listElements[i].addEventListener("mouseout", function(event) {
-    //         if (this.children.length > 0) {
-    //             var elementToHide = this.children[0];
-    //             elementToHide.style.display = "none";
-    //         }
-    //
-    //     });
-    // }
-    //
+    var imageBox = document.querySelectorAll(".image_box");
+
+    for (var i = 0; i < imageBox.length; i++) {
+      
+        imageBox[i].addEventListener("mouseover", function(event) {
+            this.firstElementChild.style.display = "none";
+        }); // zamkniecie eventu mouseenter
+
+        imageBox[i].addEventListener("mouseout", function(event) {
+            this.firstElementChild.style.display = "initial";
+
+        }); // zamkniecie eventu mouseout
+    } // zamkniecie for
+
+
 
 
 
@@ -29,3 +25,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 });
+// ------------------------DROP DOWN MENU-----------------------------------
+// var listElements = document.querySelectorAll(".drop_down ul li");
+//
+// for (var i = 0; i < listElements.length; i++) {
+//     listElements[i].addEventListener("mouseover", function(event) {
+//         if (this.children.length > 0) {
+//             var elementToShow = this.children[0];
+//             elementToShow.style.display = "block";
+//         }
+//     });
+//
+//     listElements[i].addEventListener("mouseout", function(event) {
+//         if (this.children.length > 0) {
+//             var elementToHide = this.children[0];
+//             elementToHide.style.display = "none";
+//         }
+//
+//     });
+// }
+//
